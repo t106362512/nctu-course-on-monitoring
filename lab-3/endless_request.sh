4881a8bed4a5d8d8bd2d7c85a0fdf7eaadda0fbc
jeremy-wang-lin <51841335+jeremy-wang-lin@users.noreply.github.com>
2022-05-16 22:43:50 +0800

    Refine lab-3 to add grafana, add alert rule, and refine script

--- lab-3/endless_request.sh
+++ lab-3/endless_request.sh
@@ -4,13 +4,11 @@ set -x
 
 while :
 do
-  curl http://localhost:8000/foo
-  curl http://localhost:8000/foo
-  curl http://localhost:8000/foo
+  curl http://localhost:8000/foo &
+  curl http://localhost:8000/foo &
+  curl http://localhost:8000/foo &
 
-  curl http://localhost:8000/bar
-  curl http://localhost:8000/bar
-  curl http://localhost:8000/bar
+  curl http://localhost:8000/bar &
   
   sleep 1
 done
