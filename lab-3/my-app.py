0412d022bec5153bce440d75aea3713592e6f844
ChihChia Wang <cwang@cs.nctu.edu.tw>
2022-05-19 13:06:17 +0800

    feat(lab-3): ✨ Update author and author_email

--- lab-3/my-app.py
+++ lab-3/my-app.py
@@ -8,8 +8,8 @@ REQUESTS_COUNTER = Counter('myapp_requests', 'Demo counter metric to record requ
 
 app_info = {
   'app': 'myapp',
-  'author' : 'Jeremy Lin',
-  'author_email': 'alucard.lin@gmail.com',
+  'author' : '310551017 王志嘉',
+  'author_email': 'cwang.cs10@nycu.edu.tw',
   'version' : '0.0.1'
 }
 
